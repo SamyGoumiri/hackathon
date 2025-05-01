7add32fc29903f393049b62e7a29268d92b041ba
Ahmed Allali <ahmed0702ff@gmail.com>
2025-05-02 00:22:28 +0100

    kh

---
+++ content/chatbot/main.jsx
@@ -0,0 +1,7 @@
+
+// This file is part of the Chatbot project 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
