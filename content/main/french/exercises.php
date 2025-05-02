9f671f7d076648e9f767bc3b469c7e0f33d3c1c8
Ahmed Allali <ahmed0702ff@gmail.com>
2025-05-02 21:25:54 +0100

    k

--- content/main/french/exercises.php
+++ content/main/french/exercises.php
@@ -19,7 +19,7 @@ require_once '../../../database/connect.php';
             <span id="questionCounter" class="text-sm text-gray-500">Question 1</span>
         </div>
 
-        <a href="german.php" class="flex items-center color :violet; hover:text-violet-600 mb-4">
+        <a href="french.php" class="flex items-center color :violet; hover:text-violet-600 mb-4">
             <svg class="w-5 h-5 mr-1" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L4.414 9H18a1 1 0 110 2H4.414l3.293 3.293a1 1 0 010 1.414z" clip-rule="evenodd" />
             </svg>
@@ -42,10 +42,13 @@ require_once '../../../database/connect.php';
 
 
         let vocabQuestions = [
-            { question: "What does this word mean in french?", word: "Train", image: "https://img.icons8.com/dusk/64/train.png", options: ["Auto", "Zug", "Flugzeug", "Fahrrad"], answer: "Zug" },
-            { question: "What does this word mean in German?", word: "Passport", image: "https://img.icons8.com/dusk/64/passport.png", options: ["Führerschein","Ticket","Pass", "Karte"], answer: "Pass" }
+            { question: "What does this word mean ?", word: "Bonjour", image: "https://img.icons8.com/ios-filled/100/man-raising-hand-icon.png", options: ["Hello", "Goodnight", "Thankyou", "please"], answer: "Hello" },
+            { question: "what is the french word of 'I' ? ", word: "", image: "https://img.icons8.com/ios-filled/100/i-pronoun.png", options: ["Je", "Tu", "Il", "Nous"], answer: "Je" }
+            { question: "What does this word mean ?", word: "Bonjour", image: "https://img.icons8.com/ios-filled/100/man-raising-hand-icon.png", options: ["Hello", "Goodnight", "Thankyou", "please"], answer: "Hello" },
+            { question: "what is the french word of 'I' ? ", word: "", image: "https://img.icons8.com/ios-filled/100/i-pronoun.png", options: ["Je", "Tu", "Il", "Nous"], answer: "Je" }
+
         ];
-        
+
         let fillQuestions = [
             { question: "Fill in the blank: Ich ___ müde.", answer: "bin" },
             { question: "Fill in the blank: Er ___ nach Hause.", answer: "geht" }
