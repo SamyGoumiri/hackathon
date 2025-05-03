51f89d7211f5a656cd8b2b078bae619df84b07dd
Ahmed Allali <ahmed0702ff@gmail.com>
2025-05-03 04:18:54 +0100

    Update exercises.php

--- content/main/spanish/exercises.php
+++ content/main/spanish/exercises.php
@@ -47,17 +47,11 @@ require_once '../../../database/connect.php';
         let vocabQuestions = [
 
             {question: "What does 'Hola' mean?",word: "Hola",options: ["Hello", "Goodbye", "Thank you", "Please"],answer: "Hello",image: "https://img.icons8.com/ios-filled/100/man-raising-hand-icon.png"},//done
-<<<<<<< Updated upstream
             {question: "What is the Spanish word for 'I'?",word:"",options: ["Yo", "Tú", "Él", "Nosotros"],answer: "Yo",image: "https://img.icons8.com/ios/100/i-pronoun.png"},//done
             {question: "How do you say 'apple' in Spanish?",word:"Apple",options: ["Manzana", "Plátano", "Naranja", "Uva"],answer: "Manzana",image: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/100/external-apple-autumn-season-flaticons-lineal-color-flat-icons-3.png"},//done
             {question: "Translate to Spanish: 'How are you?'",word:"",options: ["¿Cómo estás?", "¿Dónde está la manzana?", "Eres un chico.", "Estoy cansado."],answer: "¿Cómo estás?",image: "https://img.icons8.com/ios-filled/100/communication.png"},
             {question: "Which one means 'I am' in Spanish?",word:"I am",options: ["Yo soy", "Tú eres", "Él es", "Nosotros somos"],answer: "Yo soy",image: "https://img.icons8.com/ios/100/i-pronoun.png"},//done
-=======
-            {question: "What is the Spanish word for 'I'?",word:"me",options: ["Yo", "Tú", "Él", "Nosotros"],answer: "Yo",iamge: "https://img.icons8.com/color/100/i-skin-type-4.png"},//done
-            {question: "How do you say 'apple' in Spanish?",word:"Apple",options: ["Manzana", "Plátano", "Naranja", "Uva"],answer: "Manzana",iamge: "https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/100/external-apple-smart-farm-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"},//done
-            {question: "Translate to Spanish: 'How are you?'",word:"How are you?",options: ["¿Cómo estás?", "¿Dónde está la manzana?", "Eres un chico.", "Estoy cansado."],answer: "¿Cómo estás?",iamge: "https://img.icons8.com/ios-filled/100/man-raising-hand-icon.png"},
-            {question: "Which one means 'I am' in Spanish?",word:"I am",options: ["Yo soy", "Tú eres", "Él es", "Nosotros somos"],answer: "Yo soy",image: "https://img.icons8.com/color/100/i-skin-type-4.png"},//done
->>>>>>> Stashed changes
+
         ];
 
         let fillQuestions = [
